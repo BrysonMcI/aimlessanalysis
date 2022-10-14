126fe96f9a3be655a62d7fab27179abda6f68a43
Bryson McIver <brysonmciver@gmail.com>
2022-10-13 21:40:58 -0500

    Update google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,29 @@ module.exports = {
         pageTransitionDelay: 0,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-163406573-1", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Defaults to https://www.googletagmanager.com
+          origin: "https://aimlessanalysis.com/",
+        },
+      },
+    }
   ],
 }
